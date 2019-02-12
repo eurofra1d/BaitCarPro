7d9e07482a8bec90a5a62e2b23d31d69bbf5cb51
eurofra1d <39222778+eurofra1d@users.noreply.github.com>
2019-02-12 16:07:59 -0500

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,29 @@
+--[[
+__________        .__  __   _________             __________                
+\______   \_____  |__|/  |_ \_   ___ \_____ ______\______   \_______  ____  
+ |    |  _/\__  \ |  \   __\/    \  \/\__  \\_  __ \     ___/\_  __ \/  _ \ 
+ |    |   \ / __ \|  ||  |  \     \____/ __ \|  | \/    |     |  | \(  <_> )
+ |______  /(____  /__||__|   \______  (____  /__|  |____|     |__|   \____/ 
+		\/      \/                  \/     \/                               
+		
+ -------------------------------------
+|		 	 BaitCarPro v1.0		  |
+|		  Written by EURoFRA1D		  |
+ -------------------------------------
+
+ -------------------------------------
+|	  **  DO NOT REDISTRIBUTE  **     |
+|   **  DO NOT REMOVE MY CREDITS  **  |
+|   ** DO NOT CHANGE FILE NAMES **    |
+ -------------------------------------
+
+]]
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Requires NativeUI (Lua) for menu
+dependency 'NativeUI'
+client_script "@NativeUI/NativeUI.lua"
+
+client_script 'bcpcl.lua'
+server_script 'bcpsv.lua'
