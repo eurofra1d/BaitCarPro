afa2a4a14d0a0d15d45ff9778812262fb7b1b424
Legacy <brandonkershaw@gmail.com>
2022-12-18 08:14:35 -0500

    Created fxmanifest.lua
    Updates and replaces deprecated __resource.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'EURoFRAID'
+description 'BaitCarPro by EURoFRA1D'
+version '1.0.0'
+repository 'https://github.com/eurofra1d/BaitCarPro'
+
+server_scripts {
+	'bcpsv.lua'
+}
+
+client_scripts {
+	'@NativeUI/NativeUI.lua',
+	'bcpcl.lua'
+}
+
+dependency 'NativeUI'
